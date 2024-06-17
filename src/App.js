4519e5e1cb3019f65c479d477ab1df3a6cfb5c95
EduardoRubio <rubioeduardo41@gmail.com>
2024-06-16 17:51:55 -0700

    commit bien

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import Pokeapi from './Pokeapi';
+
+function App() {
+  return (
+    <div class='Pokeapi'>
+        <Pokeapi />
+      </div>
+  );
+}
+
+export default App;
